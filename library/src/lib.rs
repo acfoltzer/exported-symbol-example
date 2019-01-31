65bd5234b1b9eeb6c2d1e2193d2cc7e07e0931c5
Adam C. Foltzer <acfoltzer@fastly.com>
2019-01-31 15:21:03 -0800

    weird trait object variant

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,20 +1,25 @@
 use libloading::{Library, Symbol};
-use std::ffi::OsStr;
-
-pub fn load_and_run<P: AsRef<OsStr>>(filename: P) {
-    // this makes it work without printing an extra message:
-    // unsafe {
-    //     std::ptr::read_volatile(module_callback as *const extern "C" fn());
-    // }
-
-    let module = Library::new(filename).unwrap();
-    unsafe {
-        let module_func: Symbol<unsafe extern "C" fn()> = module.get(b"module_func").unwrap();
-        module_func();
-    }
-}
 
 #[no_mangle]
 pub extern "C" fn module_callback() {
     println!("Hello from the callback!");
 }
+
+pub trait T {
+    fn ensure_linked(&self) {
+        println!("{:?}", S {});
+    }
+
+    fn load_and_run(&self, filename: &str) {
+        let module = Library::new(filename).unwrap();
+        unsafe {
+            let module_func: Symbol<unsafe extern "C" fn()> = module.get(b"module_func").unwrap();
+            module_func();
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct S {}
+
+impl T for S {}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
+use library::{S, T};
+
 fn main() {
-    // if this call is uncommented, the example works, but prints an extra message:
-    // library::module_callback();
-
-    // this makes it work without printing an extra message:
-    // unsafe {
-    //     std::ptr::read_volatile(library::module_callback as *const extern "C" fn());
-    // }
-
-    library::load_and_run("target/debug/libmodule.so");
+    let s = S {};
+    let t = &s as &T;
+    t.load_and_run("target/debug/libmodule.so");
 }
-
-// if this definition is uncommented, the example works:
-// #[no_mangle]
-// pub extern "C" fn module_callback() {
-//     println!("Hello from the callback!");
-// }
